02e32e8ce22c5ad5d1a6feca8dc958ae6f2b4978
JavierIRamirezA <javier.ramireza@usm.cl>
2025-07-04 19:24:41 -0400

    Update Main.java
    Se cambia fxmlLoader para que obtenga la ruta del archivo fxml

--- src/main/java/cl/ufsm/dpoo/sistemaparking/Main.java
+++ src/main/java/cl/ufsm/dpoo/sistemaparking/Main.java
@@ -9,7 +9,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("parking-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/cl/ufsm/dpoo/sistemaparking/parking-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("Sistema de Parking - JavaFX");
         stage.setScene(scene);
