dc30e331d56794b6daae7bd3086ae094ebfafab7
ssobarzo <sebastian.sobarzo@usm.cl>
2025-07-04 22:33:58 -0400

    documentacion

--- src/main/java/cl/ufsm/dpoo/sistemaparking/EspacioParking.java
+++ src/main/java/cl/ufsm/dpoo/sistemaparking/EspacioParking.java
@@ -1,33 +1,58 @@
 package cl.ufsm.dpoo.sistemaparking;
-
+/**
+ * Espacio de aparcamiento en un estacionamiento.
+ * */
 public class EspacioParking {
+    /** Numero de espacio.*/
     private int numero;
+    /** Estado del espacio.*/
     private boolean ocupado;
+    /** Vehiculo el cual esta ocupando el espacio.*/
     private Vehiculo vehiculo;
 
+    /**
+     * Metodo constructor, inicializa con un numero y con estado ocupado y sin vehiculo.
+     * @param numero numero del espacio.
+     * */
     public EspacioParking(int numero) {
         this.numero = numero;
         this.ocupado = false;
         this.vehiculo = null;
     }
-
+    /** Metodo para conocer el estado
+     * @return True si el espacio esta libre.
+     * */
     public boolean isLibre() {
         return !ocupado;
     }
 
+    /**
+     * Metodo para ocupar el espacio.
+     * @param vehiculo Vehiculo el cual ocupara el espacio.
+     * */
     public void ocupar(Vehiculo vehiculo) {
         this.vehiculo = vehiculo;
         this.ocupado = true;
     }
-
+    /**
+     * Metodo para liberar un espacio.
+     * */
     public void liberar() {
         this.vehiculo = null;
         this.ocupado = false;
     }
-
+    /**
+     * Metodo para obtener el numero del espacio.
+     * @return Numero del espacio.
+     * */
     public int getNumero() {
         return numero;
     }
+
+    /**
+     * Metodo para obtener el vehiculo aparcado.
+     * @return Vehiculo aparcado.
+     * */
 
     public Vehiculo getVehiculo() {
         return vehiculo;
--- src/main/java/cl/ufsm/dpoo/sistemaparking/Ticket.java
+++ src/main/java/cl/ufsm/dpoo/sistemaparking/Ticket.java
@@ -1,25 +1,39 @@
 package cl.ufsm.dpoo.sistemaparking;
 import java.time.LocalDateTime;
 
+/** Ticket entregado a los vehiculos que ingresan al sistema.*/
+
 public class Ticket {
+    /** Numero de ticket.*/
     private int numero;
+    /** Vehiculo al que le corresponde el ticket.*/
     private Vehiculo vehiculo;
+    /** Fecha de entrada del vehiculo.*/
     private LocalDateTime horaEntrada;
 
+    /** Metodo constructor, inicializa con un numero, vehiculo y fecha.
+     * @param numero Numero de ticket.
+     * @param vehiculo Vehiculo al que le corresponde el ticket.*/
     public Ticket(int numero, Vehiculo vehiculo) {
         this.numero = numero;
         this.vehiculo = vehiculo;
         this.horaEntrada = LocalDateTime.now();
     }
 
+    /** Metodo que obtiene el numero del ticket.
+     * @return Numero del ticket.*/
     public int getNumero() {
         return numero;
     }
 
+    /** Metodo que obitene el vehiculo asociado al ticket.
+     * @return Vehiculo asociado al ticket.*/
     public Vehiculo getVehiculo() {
         return vehiculo;
     }
 
+    /** Metodo que obtiene la fecha de entrada del vehiculo.
+     * @return Fecha de entrada.*/
     public LocalDateTime getHoraEntrada() {
         return horaEntrada;
     }
